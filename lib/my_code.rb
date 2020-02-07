753614d7aa5535a5a666151f262bf049af3958f7
Zac Uslianer <zacuslianer@gmail.com>
2020-02-07 18:51:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,5 +2,12 @@
 require 'pry'
 
 def map_to_negativize(source_array)
-  binding.pry
+  counter = 0 
+  new_array = []
+  while counter < source_array.length 
+  source_array[counter] * -1
+  new_array << source_array[counter] * -1
+ counter += 1
+ end
+new_array
 end
