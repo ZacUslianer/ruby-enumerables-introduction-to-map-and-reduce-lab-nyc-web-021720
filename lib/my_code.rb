96f9c94a9ce62695c916814fe5d99aa72b7a0453
Zac Uslianer <zacuslianer@gmail.com>
2020-02-07 20:36:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -67,5 +67,13 @@ true
 end
 
 def reduce_to_any_true(source_array)
- 
+ counter = 0 
+while counter < source_array.length do
+  if source_array[counter] == true
+  return true
+  end
+counter += 1
+end
+true
+end
 
